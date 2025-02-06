57c72e32661778378a8f441131b1899aca745045
jadchahine111 <jadalichahine@gmail.com>
2025-02-06 22:35:51 +0200

    Minor Bug Changes

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Category;
-
+use App\Models\Subcategory; 
 use Illuminate\Support\Facades\Validator; 
 use App\Models\Blog;
 
